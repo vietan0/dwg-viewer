ccec5881de0de431910223195a1b0da5aff15aa3
Viet An <tiktaktictac269@gmail.com>
2025-11-23 18:02:34 +0700

    feat: add custom object using SceneBuilder

--- wwwroot/viewer.js
+++ wwwroot/viewer.js
@@ -16,13 +16,59 @@ async function getAccessToken(callback) {
 
 export function initViewer(container) {
   return new Promise((resolve, _reject) => {
-    Autodesk.Viewing.Initializer({ env: 'AutodeskProduction', getAccessToken }, () => {
+    Autodesk.Viewing.Initializer({ env: 'AutodeskProduction', getAccessToken }, async () => {
       const config = {
-        extensions: ['Autodesk.DocumentBrowser', 'AreaDetection'],
+        extensions: ['Autodesk.ModelStructure'],
       };
+
       const viewer = new Autodesk.Viewing.GuiViewer3D(container, config);
       viewer.start();
       viewer.setTheme('light-theme');
+
+      function createBufferGeometry() {
+        const geometry = new THREE.BufferGeometry();
+        const vertices = new Float32Array([
+          -1.0,
+          -1.0,
+          1.0, // v0
+          1.0,
+          -1.0,
+          1.0, // v1
+          1.0,
+          1.0,
+          1.0, // v2
+          1.0,
+          1.0,
+          1.0, // v3
+          -1.0,
+          1.0,
+          1.0, // v4
+          -1.0,
+          -1.0,
+          1.0, // v5
+        ]);
+        // itemSize = 3 because there are 3 values (components) per vertex
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        const mesh = new THREE.Mesh(geometry, material);
+
+        return mesh;
+      }
+
+      viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, async function onceLoaded() {
+        viewer.removeEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, onceLoaded);
+
+        const sceneBuilder = await viewer.loadExtension('Autodesk.Viewing.SceneBuilder');
+        const modelBuilder = await sceneBuilder.addNewModel({
+          modelNameOverride: 'My Custom Model',
+          conserveMemory: false,
+        });
+
+        const mesh = createBufferGeometry();
+        mesh.dbId = 999999;
+        modelBuilder.addMesh(mesh);
+        viewer.impl.invalidate(true, true); // Force a refresh
+      });
 
       viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, () => {
         const myDbids = viewer.getSelection();
